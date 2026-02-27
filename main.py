8396e35e9a79405331912d069ad9596abfdcdea0
fabrica11kcastelo-rgb <fabrica11k.castelo@gmail.com>
2026-02-27 13:57:25 -0300

    Update main.py
    Ooobv

--- main.py
+++ main.py
@@ -81,10 +81,21 @@ def fetch_tokens():
   Solana {
     DEXTrades(
       limit: {count: 5}
+      orderBy: {descending: Block_Time}
     ) {
+      Block {
+        Time
+      }
       Trade {
         Buy {
-          Amount
+          AmountInUSD
+          Currency {
+            Symbol
+            MintAddress
+          }
+        }
+        Sell {
+          AmountInUSD
           Currency {
             Symbol
             MintAddress
